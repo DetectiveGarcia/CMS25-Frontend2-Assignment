ba05de000210379f7fea31aec1d1550e90a5afb9
miguel <miguel.garay@hotmail.se>
2026-01-13 21:40:00 +0100

    rows object created and implemented with jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import { useReducer } from "react";
 import MovieSelector from "./components/MovieSelector";
 import "./App.css"
 
-function reducer(state, action){
+function reducer(state, action) {
 
   const { type, payload } = action;
 
   switch (type) {
     case "Toggle Seat":
-      
+
       return {
         ...state,
         seatOccupied: !state.seatOccupied
       }
-  
+
     default:
       state;
   }
@@ -23,6 +23,23 @@ function reducer(state, action){
 const initialState = {
   seatOccupied: false
 }
+
+
+function seatSelected(e) {
+  console.log(e);
+
+}
+
+const rows = {
+  row0: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  row1: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  row2: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  row3: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  row4: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  row5: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+
+}
+
 
 function App() {
 
@@ -35,14 +52,13 @@ function App() {
       <div className="container">
         <div className="screen"></div>
         <div className="row">
-          <div className="seat"></div>
-          <div className="seat"></div>
-          <div className="seat"></div>
-          <div className="seat"></div>
-          <div className="seat"></div>
-          <div className="seat"></div>
-          <div className="seat"></div>
-          <div className="seat"></div>
+          {rows.row0.map((seat) => {
+            return (
+              <div className={state.seatOccupied ? "seat" : "seat selected"} key={seat} onClick={(e) => {console.log(e);
+              }}></div>
+
+            )
+          })}
         </div>
         <div className="row">
           <div className="seat"></div>
