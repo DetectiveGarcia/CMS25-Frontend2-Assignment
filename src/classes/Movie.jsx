7c853229c9eddc9918183587f09d45087a92b680
miguel <miguel.garay@hotmail.se>
2026-01-14 17:51:12 +0100

    Fix - Took away constructor and added key word: props

--- src/classes/Movie.jsx
+++ src/classes/Movie.jsx
@@ -2,16 +2,13 @@ import React, { Component } from "react";
 
 export default class ClassComponent extends Component {
 
-    constructor(title, price, year, poster)
-    {
-        this.title = title;
-        this.price = price;
-        this.year = year;
-        this.poster = poster;
-    }
 
-  render() {
-    return <div>ClassComponent</div>;
-  }
+
+    
+
+    render() {
+      
+        return <option value={this.props.price} key={this.props.title} >{`${this.props.title} (${this.props.price}kr)`}</option>;
+    }
 }
 
