8280aa548a0a733c4abda5a239f94be8004d67ac
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-01-16 00:59:48 +0100

    Reducer gets the user movie selection

--- src/components/MovieSelector.jsx
+++ src/components/MovieSelector.jsx
@@ -1,36 +1,40 @@
-import MovieOptions from "./MovieOptions"
-import { useEffect } from "react"
+import MovieOptions from "./MovieOptions";
+import { useEffect } from "react";
 
 export default function MovieSelector({ movies, dispatch }) {
 
-    useEffect(() => {
-        
-        
-    }, [])
 
-    return (
-        <>
-            <div className="movie-container">
-                <label htmlFor="movie">Pick a movie:</label>
-                <select name="movie" id="movie">
-                    {movies && movies.map(movie => <MovieOptions {...{ movie }} />)}
-
-                </select>
-            </div>
-            <ul className="showcase">
-                <li>
-                    <div className="seat"></div>
-                    <small>N/A</small>
-                </li>
-                <li>
-                    <div className="seat selected"></div>
-                    <small>Selected</small>
-                </li>
-                <li>
-                    <div className="seat occupied"></div>
-                    <small>Occupied</small>
-                </li>
-            </ul>
-        </>
-    )
+  return (
+    <>
+      <div className="movie-container">
+        <label htmlFor="movie" >
+          Pick a movie:
+        </label>
+          <select name="movie" id="movie"  onChange={(e) => {
+                
+                dispatch({
+                    type: "Movie Selected",
+                    payload: e.target.value
+                })
+          }}>
+            {movies && movies.map((movie) => <MovieOptions {...{ movie }} />)}
+ 
+          </select>
+      </div>
+      <ul className="showcase">
+        <li>
+          <div className="seat"></div>
+          <small>N/A</small>
+        </li>
+        <li>
+          <div className="seat selected"></div>
+          <small>Selected</small>
+        </li>
+        <li>
+          <div className="seat occupied"></div>
+          <small>Occupied</small>
+        </li>
+      </ul>
+    </>
+  );
 }
