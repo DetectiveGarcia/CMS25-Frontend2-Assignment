813692702394b867cf3cd39829ab1f04cd3d5783
miguel <miguel.garay@hotmail.se>
2026-01-15 20:46:43 +0100

    Added option html tag with movie object values

--- src/components/MovieOptions.jsx
+++ src/components/MovieOptions.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieOptions = ({ movie }) => {
   return (
     <>
-
+        <option value={movie} key={movie.title} >{`${movie.title} (${movie.price}kr)`}</option>;
     </>
   )
 }
