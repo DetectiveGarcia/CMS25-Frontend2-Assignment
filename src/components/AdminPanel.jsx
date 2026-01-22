3183f6e4da2059e07beb7552df2aa3671792629d
miguel <miguel.garay@hotmail.se>
2026-01-22 20:56:59 +0100

    Add home link

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router';
 
 const AdminPanel = () => {
   return (
-    <h1>AdminPanel</h1>
+    <main>
+
+      <h1>AdminPanel</h1>
+      <NavLink to="/" end>
+        Home
+      </NavLink>
+    </main>
   )
 }
 
