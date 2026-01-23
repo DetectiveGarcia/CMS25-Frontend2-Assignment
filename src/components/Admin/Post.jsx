a42fb8dac7b4c83ca96dacd85185621c8bd58cb1
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-01-23 20:32:23 +0100

    JSX created

--- src/components/Admin/Post.jsx
+++ src/components/Admin/Post.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export const Post = () => {
   return (
-    <div>Post</div>
-  )
-}
+    <section>
+      <h2>Submit a movie</h2>
+      <form>
+        <div className="form-field">
+          <label htmlFor="name">Name </label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div className="form-field">
+          <label htmlFor="id">Price </label>
+          <input type="number" name="id" id="id" />
+        </div>
+        <div className="form-field">
+          <label htmlFor="id">Poster </label>
+          <input type="text" name="id" id="id" />
+        </div>
+        <input type="submit" value="Send" id="form-button" />
+      </form>
+    </section>
+  );
+};
