12069229bbbd1a19ef5fa35f4968d315a2da823c
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-01-23 21:05:26 +0100

    JSX created

--- src/components/Admin/Put.jsx
+++ src/components/Admin/Put.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export const Put = () => {
   return (
-    <div>Put</div>
-  )
-}
+    <section>
+      <h2>Update a movie</h2>
+      <form>
+      <select name="" id="">
+        <option value="">Hello</option>
+      </select>
+        <div className="form-field">
+          <label htmlFor="name">Name </label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div className="form-field">
+          <label htmlFor="id">Price </label>
+          <input type="number" name="id" id="id" />
+        </div>
+        <div className="form-field">
+          <label htmlFor="id">Poster </label>
+          <input type="text" name="id" id="id" />
+        </div>
+        <input type="submit" value="Update" id="form-button" />
+      </form>
+    </section>
+  );
+};
