37d549d756c3f294b57c8e1365042eee6f4d8b01
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-01-23 17:38:51 +0100

    Deleted colons

--- src/components/Admin/Get.jsx
+++ src/components/Admin/Get.jsx
@@ -8,11 +8,11 @@ export const Get = () => {
       <h2>Get a movie</h2>
       <form>
         <div id="form-id">
-          <label htmlFor="id">ID: </label>
+          <label htmlFor="id">ID </label>
           <input type="text" name="id" id="id" />
         </div>
         <div id="form-name">
-          <label htmlFor="name">Name: </label>
+          <label htmlFor="name">Name </label>
           <input type="text" name="name" id="name" />
         </div>
         <input type="submit" value="Send" id="form-button" />
