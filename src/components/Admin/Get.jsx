4544a382425954ebdab6c47214ced158e2db57d9
miguel <miguel.garay@hotmail.se>
2026-01-23 03:38:53 +0100

    CRUD components created

---
+++ src/components/Admin/Get.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Get = () => {
+  return (
+    <div>Get</div>
+  )
+}
---
+++ src/components/Admin/Put.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Put = () => {
+  return (
+    <div>Put</div>
+  )
+}
