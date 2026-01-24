44e89006e47e163f53d7a5f4f829c0c6ffe1d07c
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-01-24 21:35:23 +0100

    implemented Deletion Logic

--- src/components/Admin/Delete.jsx
+++ src/components/Admin/Delete.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export const Delete = ({ movies }) => {
+export const Delete = ({ movies, automaticSelectedMovie }) => {
+  const [movieSelected, setMovieSelected] = useState(null);
+
+  async function deleteAction(formData) {
+    const confirmDeletion = confirm("Are you sure?");
+
+    if (!confirmDeletion) {
+      return;
+    }
+
+    const movieTitle = formData.get(
+      `${movieSelected?.title ?? automaticSelectedMovie.title}`,
+    );
+
+    const movie = movies.find((m) => m.title == movieTitle);
+
+    try {
+      await fetch(`https://localhost:7194/api/Movies/${movie.id}`, {
+        method: "DELETE",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <section>
       <h2>Delete a movie</h2>
-      <form>
-        <select name="" id="">
-          {movies.map(m => <option key={m.title}>{m.title}</option>)}
+      <form action={deleteAction}>
+        <select
+          name={movieSelected?.title ?? automaticSelectedMovie.title}
+          id=""
+          onChange={(e) => {
+            setMovieSelected(movies.find((m) => m.title === e.target.value));
+          }}
+        >
+          {movies.map((m) => (
+            <option key={m.title}>{m.title}</option>
+          ))}
         </select>
 
         <input type="submit" value="Delete" id="form-button" />
