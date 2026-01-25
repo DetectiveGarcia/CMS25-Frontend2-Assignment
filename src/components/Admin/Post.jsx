c9f6e45db9ba3e94fd885e278fcb22f796fc2d2d
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-01-25 21:01:12 +0100

    Implemented fetch logic

--- src/components/Admin/Post.jsx
+++ src/components/Admin/Post.jsx
@@ -1,21 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Post = ({ movies }) => {
+
+  async function submitAMovie(formData) {
+
+
+    const movieTitle = formData.get("name");
+    const moviePrice = formData.get("price");
+    const moviePoster = formData.get("poster");
+
+    console.log(movieTitle);
+    console.log(moviePrice);
+    console.log(moviePoster);
+
+    /*
+      Stranger things
+      190
+      https://m.media-amazon.com/images/I/81DrD8zq1XL._AC_SY300_SX300_QL70_ML2_.jpg
+    */
+    try {
+      
+      const response = await fetch("https://localhost:7194/api/Movies", {
+        method: "POST",
+        body: JSON.stringify({
+          title: movieTitle,
+          price: moviePrice,
+          poster: moviePoster
+        }),
+        headers: {
+          "Content-type": "application/json"
+        }
+      })
+
+      if(!response.ok){
+        throw new Error("Submission not possible")
+      }
+
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+
+    alert("Movie added");
+    window.location.reload();
+  }
+
   return (
     <section>
       <h2>Submit a movie</h2>
-      <form>
+      <form action={submitAMovie}>
         <div className="form-field">
           <label htmlFor="name">Name </label>
           <input type="text" name="name" id="name" />
         </div>
         <div className="form-field">
-          <label htmlFor="id">Price </label>
-          <input type="number" name="id" id="id" />
+          <label htmlFor="price">Price </label>
+          <input type="number" name="price" id="price" />
         </div>
         <div className="form-field">
-          <label htmlFor="id">Poster </label>
-          <input type="text" name="id" id="id" />
+          <label htmlFor="poster">Poster </label>
+          <input type="text" name="poster" id="poster" />
         </div>
         <input type="submit" value="Send" id="form-button" />
       </form>
