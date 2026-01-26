4dbf5cb43abee31d5adc6cdbeb8e35a3291b0cb2
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-01-27 00:09:57 +0100

    new css style

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,42 +1,38 @@
-import React from 'react'
+import React from "react";
 
 export const BookingForm = () => {
+  const sendBooking = (formData) => {
+    const inputName = formData.get("name");
+    const inputTelephone = formData.get("telephone");
 
-    const sendBooking = (formData) => {
-        
+    console.log(inputName);
+    console.log(inputTelephone);
+  };
 
-
-        const inputName = formData.get("name");
-        const inputTelephone = formData.get("telephone");
-        
-
-        console.log(inputName);
-        console.log(inputTelephone);
-        
-
-    }
-
-
-    
-    // class Movie{
-    //     constructor(poster, price, title, year){
-    //         this.poster = poster;
-    //         this.price = price;
-    //         this.title = title;
-    //         this.year = year;
-    //     }
-    // }
-
-
+  // class Movie{
+  //     constructor(poster, price, title, year){
+  //         this.poster = poster;
+  //         this.price = price;
+  //         this.title = title;
+  //         this.year = year;
+  //     }
+  // }
 
   return (
-    <form action={sendBooking}  >
-        <label htmlFor="name" >Namn: </label>
-        <input type="text" name='name' id='' />
+    <>
+      <form action={sendBooking} id="booking-form">
+        <h3 style={{ alignSelf: "center"}}>Book seats</h3>
+        <div className="form-field">
+          <label htmlFor="name">Namn</label>
+          <input type="text" name="name" id="" />
+        </div>
 
-        <label htmlFor="telephone">Telefonnummer: </label>
-        <input type="text" name="telephone" id="" />
+        <div className="form-field">
+          <label htmlFor="telephone">Tlf. nr</label>
+          <input type="text" name="telephone" id="" />
+        </div>
         <input type="submit" value="Send" />
-    </form>
-  )
-}
+      </form>
+    </>
+  );
+};
