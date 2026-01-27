349b5cd31fa2f29a9a12c43aa16bcf6669e85a68
miguel <miguel.garay@hotmail.se>
2026-01-27 13:29:35 +0100

    logical AND replace with turnery on className

--- src/components/Seat.jsx
+++ src/components/Seat.jsx
@@ -6,7 +6,7 @@ export default function Seat({ dispatch, seatId, selected, seat }) {
 
   return (
     <div
-      className={`${selected ? "seat selected" : "seat"} ${seat.occupied && "occupied"}`} 
+      className={`${selected ? "seat selected" : "seat"} ${seat.occupied ? "occupied" : ""}`} 
       onClick={() => {
         if(!seat.occupied){
           dispatch({
